e49c0f1d2194875d45d393dd6522601453de2ee4
JumpyJacko <bobjacklee27@gmail.com>
2022-01-17 13:59:41 +1100

    Added QoL features and reworked backend
    Added a progress bar for the fetching of pages and added a feh execution. Also added a homepage where it is planned to be able to execute other actions. Reworked the code base, switched "type": "module" and moved the manga fetching code to a different javascript file. index.js now only includes the homepage.

---
+++ src/fetch-manga-by-title.js
@@ -0,0 +1,102 @@
+import { Manga, login } from 'mangadex-full-api';
+import { exec, spawnSync } from 'child_process';
+
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+
+import cliProgress from 'cli-progress';
+const bar = new cliProgress.SingleBar({
+    format: chalk.yellow('Fetching Pages') +  ' | [{bar}] | {percentage}% | {value}/{total} Pages',
+    barCompleteChar: '=',
+    barIncompleteChar: ' ',
+    hideCursor: true
+});
+
+import chalk from 'chalk';
+
+import options from './options.json' assert { type: 'json' };
+
+export async function fetchMangaByTitle() {
+    let manga_title = prompt(`Manga Title: `);
+
+    login(options.username, options.password).then(async () => {
+        let manga = await Manga.getByQuery(manga_title);
+        let chapters = await manga.getFeed({ translatedLanguage: ['en'] }, true);
+        
+        console.log(`Fetching from ` + chalk.yellow.underline(manga.title) + `...`)
+
+        let check_chapters = prompt(`List Chapters? (y/n) `);
+
+
+        let chapter_numbers = [];
+        for (let i = 0; i < Object.keys(chapters).length; i++) {
+            chapter_numbers.push(chapters[i].chapter)
+        }
+        let sorted = chapter_numbers.sort((a, b) => {
+            return a - b;
+        });
+        
+        if (check_chapters == 'y') {
+            console.log(sorted);
+        }
+
+        let manga_chapter = prompt(`Chapter Number: `);
+     
+        let chapter;
+        for (let j = 0; j < sorted.length; j++) {
+            if (chapters[j].chapter == manga_chapter) {
+                chapter = chapters[j]
+            } else {
+                continue;
+            }
+        }
+
+        let pages = await chapter.getReadablePages();
+        
+        function read() {
+            let read_check = prompt('Read? (y/n) ');
+            
+            if (read_check == 'y') {
+                exec('feh -.n src/manga');
+                console.log('Close reading window and press any key to exit...');
+                spawnSync('read _', {shell: true, stdio: [0, 1, 2]});
+            }
+        }
+
+        function download_pages() {
+            exec('find -type f -name \'*page*\' -delete')
+            bar.start(pages.length, 0);
+            let k;
+            let counter = 0;
+            for (k = 0; k < pages.length; k++) {
+                let id = '.';
+                let l;
+                for (l = 1; l < k; l++) {
+                    id = id + ".";
+                }
+                
+                exec(`curl -o ./src/manga/page-${id}.png ${pages[k]}`, (error, stdout, stderr) => {
+                    if (error) {
+                        console.log(error);
+                    } else if (stderr) {
+                        bar.increment();
+                        counter++;
+                        if (counter == pages.length) {
+                            bar.stop();
+                            read();
+                        }
+                    } else {
+                        console.log(stdout);
+                    }
+                });
+            }
+        }
+        let check = prompt(`Fetching from ` + chalk.yellow.underline(manga.title) + ` at Chapter ` + chalk.yellow.underline(chapter.chapter) + `. Is this correct? (y/n) `)
+        
+        if (check == 'y') {
+            download_pages();
+        } else {
+            console.log('Fetch aborted.')
+        }
+    })
+}
--- src/index.js
+++ src/index.js
@@ -1,67 +1,27 @@
-const MFA = require('mangadex-full-api');
-const exec = require('child_process').exec;
+#!/usr/bin/env node
 
-const prompt = require('prompt-sync')();
+import chalk from 'chalk';
+import figlet from 'figlet';
 
-const options = require('./options.json');
+import { fetchMangaByTitle } from './manga-fetch.js';
 
-let manga_title = prompt('Manga Title: ');
+const init = () => {
+    console.log(chalk.yellow(
+        figlet.textSync('manga - cli', {
+            font: 'Big',
+            horizontalLayout: 'default',
+            verticalLayout: 'default'
+        })
+    ));
+    console.log(chalk.grey(`\nA MangaDex client from the comfort of your terminal!\n`));
+    console.log(chalk.grey(`Currently does not support MangaPlus, it is noted that\nthe program will crash when querying MangaPlus manga.\n`))
+    console.log(chalk.grey(`Only works if you know exactly what you want to read\nbecause there is no search function.`));
+}
 
-MFA.login(options.username, options.password).then(async () => {
-    manga = await MFA.Manga.getByQuery(manga_title);
-    chapters = await manga.getFeed({ translatedLanguage: ['en'] }, true);
-    
-    console.log(`Fetching from ${manga.title}...`)
+const run = async () => {
+    // CLI introduction
+    init();
+    fetchMangaByTitle();
+}
 
-    let check_chapters = prompt("List Chapters? (y/n) ");
-
-    if (check_chapters == 'y') {
-        let chapter_numbers = [];
-        for (i = 0; i < Object.keys(chapters).length; i++) {
-            chapter_numbers.push(chapters[i].chapter)
-        }
-        sorted = chapter_numbers.sort((a, b) => {
-            return a - b;
-        });
-        console.log(sorted);
-    }
-
-    let manga_chapter = prompt('Chapter Number: ');
- 
-    for (i = 0; i < sorted.length; i++) {
-        if (chapters[i].chapter == manga_chapter) {
-            chapter = chapters[i]
-        } else {
-            continue;
-        }
-    }
-
-    pages = await chapter.getReadablePages();
-
-    function download_pages() {
-        exec('find -type f -name \'*page*\' -delete')
-        for (i = 0; i < pages.length; i++) {
-            let id = '.';
-            for (j = 1; j < i; j++) {
-                id = id + ".";
-            }
-            exec(`curl -o ./src/manga/page-${id}.png ${pages[i]}`, (error, stdout, stderr) => {
-                if (error) {
-                    console.log(error)
-                } else if (stderr) {
-                    console.log(`Fetched page`);
-                } else {
-                    console.log(stdout);
-                }
-            });
-        }
-    }
-    let check = prompt(`Fetching from ${manga.title} at Chapter ${chapter.chapter}. Is this correct? (y/n) `)
-    
-    if (check == 'y') {
-        download_pages()
-    } else {
-        console.log('Fetch failed.')
-    }
-
-}).catch(console.error);
+run();
